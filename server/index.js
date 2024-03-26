d593df71b9d26a85d4e811882e4ae182101037b8
Ilangkoon I.M.C.S.B. <chamathshymantha@gmail.com>
2024-03-26 15:35:49 +0530

    Refactor code for improved performance and readability

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+//ROUTES//
+
+//create a todo
+
+app.post("/todos",async(req,res) => {
+    try{
+
+        console.log(req.body);
+
+} catch (err){
+    console.error(err.message);
+}
+});
+//get all todo
+
+//get to do
+
+//update a todo
+
+//delete a todo
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
 });
